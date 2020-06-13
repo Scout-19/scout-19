a620510f674608c1387ac944381f73226638311a
Kanta Tsuchimoto <kt.tsuchi000@gmail.com>
2020-06-13 21:57:23 +0900

    Edit video list page, adjust appearance

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,55 +1,201 @@
 <template>
-  <div class="video">
+
+  <div class="video" ref="video_page" height="1000px">
     <v-container>
-      <BasicVideoFrame
-        class='pb-5'
-        :icon='icon'
-        :name='name'
-        :video_path='video_path'
-        :video_title='video_title'
-        :good='good'
-        :comment='comment'
-        :hash_tags='hash_tags'
-        :thumbnail='thumbnail'
-      />
+      <v-row dense>
+        <v-col>
+          <v-text-field v-model="search_text"
+                        label="検索"
+                        prepend-inner-icon="mdi-magnify"
+                        @keydown.enter="search(search_text)">
+          </v-text-field>
+        </v-col>
+        <v-col cols="1">
+          <v-btn style="bottom: -12px"
+                 @click="search(search_text)">
+            検索
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-row justify="start" dense>
+            <BasicVideoFrame class='pb-5'
+                             v-for="(video, i) in videos"
+                             :key="i"
+                             :icon='video.icon'
+                             :name='video.name'
+                             :video_ref='video.video_ref'
+                             :title='video.title'
+                             :good='video.good'
+                             :hash_tags='video.hash_tags'
+                             :thumbnail_refs='video.thumbnail_refs'
+                             :overview='video.overview'
+                             :contributor='video.contributor'
+                             @search="search"/>
+          </v-row>
+        </v-col>
+
+      </v-row>
     </v-container>
   </div>
+
 </template>
 
 <script>
-import BasicVideoFrame from '@/components/BasicVideoFrame.vue'
 
-import { mapGetters } from 'vuex'
+  import { mapGetters } from 'vuex'
 
-export default {
-  name: 'Video',
+  import BasicVideoFrame from '@/components/BasicVideoFrame.vue'
+  import firebase from 'firebase/app'
 
-  components: {
-    BasicVideoFrame,
-  },
+  export default {
+    name: 'Movie',
 
-  data: () => ({
-    dataLoaded: false,
+    components: {
+      BasicVideoFrame
+    },
 
-    icon: 'https://randomuser.me/api/portraits/women/4.jpg',
-    name: 'NAME',
-    video_title: 'TITLE',
-    video_path: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg',
-    good: '100',
-    comment: '10',
-    hash_tags: ['#tag', '#tag', '#tag'],
-    thumbnail: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg'
-  }),
+    data: () => ({
+      dataLoaded: false,
+      search_text: '',
+      videos: [],
+      last_doc: null
+    }),
 
-  created() {
-    // todo: get from vuex
-    // var uid = this.getUid
-   },
+    methods: {
+      search(text) {
+        console.log(text)
+        this.videos = []
+        var ref = firebase.firestore().collection('videos')
+        if (text.match(/^#.+/)) {
+          ref.where("hash_tags", "array-contains", text)
+          .get()
+          .then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+              var data = doc.data()
+              var contributor = data.contributor
+              var user_ref = firebase
+                .firestore()
+                .collection('users')
+                .doc(contributor)
+                .collection('profile')
+                .doc('public')
+              user_ref.get().then(doc => {
+                if (doc.exists) {
+                  var profile = doc.data()
+                  data['icon'] = profile.icon
+                  data['name'] = profile.last_name + profile.first_name
+                  this.videos.push(data)
+                }
+              })
+            })
+          })
+          .catch(error => {
+            console.log('Error getting documents: ', error)
+          })
+        }
+        else {
+          ref.where("title", ">=", text)
+          .get()
+          .then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+              var data = doc.data()
+              var contributor = data.contributor
+              var user_ref = firebase
+                .firestore()
+                .collection('users')
+                .doc(contributor)
+                .collection('profile')
+                .doc('public')
+              user_ref.get().then(doc => {
+                if (doc.exists) {
+                  var profile = doc.data()
+                  data['icon'] = profile.icon
+                  data['name'] = profile.last_name + profile.first_name
+                  this.videos.push(data)
+                }
+              })
+            })
+          })
+          .catch(error => {
+            console.log('Error getting documents: ', error)
+          })
+        }
 
-  computed: {
-    ...mapGetters([
-      'getUid',
-    ]),
-  },
-}
+        return
+      },
+      push_videos() {
+        var ref = firebase.firestore().collection('videos')
+        if (!this.last_doc) {
+          ref
+            .orderBy('posting_date', 'desc')
+            .limit(1)
+            .get()
+            .then(querySnapshot => {
+              this.last_doc = querySnapshot.docs[querySnapshot.docs.length-1]
+              console.log(this.last_doc)
+              querySnapshot.forEach(doc => {
+                console.log(doc)
+                var data = doc.data()
+                var contributor = data.contributor
+                var user_ref = firebase
+                  .firestore()
+                  .collection('users')
+                  .doc(contributor)
+                  .collection('profile')
+                  .doc('public')
+                user_ref.get().then(doc => {
+                  if (doc.exists) {
+                    var profile = doc.data()
+                    data['icon'] = profile.icon
+                    data['name'] = profile.last_name + profile.first_name
+                    this.videos.push(data)
+                  }
+                })
+              })
+            })
+        }
+        else {
+          ref
+            .orderBy('posting_date', 'desc')
+            .startAfter(this.last_doc)
+            .limit(1)
+            .get()
+            .then(querySnapshot => {
+              this.last_doc = querySnapshot.docs[querySnapshot.docs.length-1]
+              querySnapshot.forEach(doc => {
+                console.log(doc)
+                var data = doc.data()
+                var contributor = data.contributor
+                var user_ref = firebase
+                  .firestore()
+                  .collection('users')
+                  .doc(contributor)
+                  .collection('profile')
+                  .doc('public')
+                user_ref.get().then(doc => {
+                  if (doc.exists) {
+                    var profile = doc.data()
+                    data['icon'] = profile.icon
+                    data['name'] = profile.last_name + profile.first_name
+                    this.videos.push(data)
+                  }
+                })
+              })
+            })
+            
+        }
+
+      },
+    },
+    created() {
+      this.push_videos()
+    },
+
+    computed: {
+      ...mapGetters(['getUid'])
+    }
+  }
+
 </script>
